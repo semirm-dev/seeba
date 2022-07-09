a60098e909bea916014fdabb4c0760771e75143b
Semir Mahovkic <semirm.dev@gmail.com>
2022-07-09 13:12:46 +0200

    seeba

--- gateway/music_test.go
+++ gateway/music_test.go
@@ -1,9 +1,47 @@
 package gateway_test
 
 import (
+	"encoding/xml"
+	"github.com/semirm-dev/seeba/gateway"
+	"github.com/semirm-dev/seeba/internal/web"
+	"github.com/stretchr/testify/assert"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 )
 
-func TestGetMusic(t *testing.T) {
+type apiResponse string
 
+type mockedSearch struct {
+}
+
+func (srch *mockedSearch) All() (interface{}, error) {
+	return "mocked music data", nil
+}
+
+func TestGetMusic(t *testing.T) {
+	router := web.NewRouter()
+	router.GET("music", gateway.GetMusic(&mockedSearch{}))
+
+	req, _ := http.NewRequest("GET", "/music", nil)
+
+	w := httptest.NewRecorder()
+	router.ServeHTTP(w, req)
+
+	if w.Code != http.StatusOK {
+		t.Fail()
+	}
+
+	respBody, err := ioutil.ReadAll(w.Body)
+	if err != nil {
+		t.Fail()
+	}
+
+	var resp apiResponse
+	if err = xml.Unmarshal(respBody, &resp); err != nil {
+		t.Fail()
+	}
+	assert.NotNil(t, resp)
+	assert.Equal(t, apiResponse("mocked music data"), resp)
 }
