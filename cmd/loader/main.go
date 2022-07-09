5d59a3a42a2a6575a36158748406bedbd0d4cebd
Semir Mahovkic <semirm.dev@gmail.com>
2022-07-09 11:11:30 +0200

    seeba

--- cmd/loader/main.go
+++ cmd/loader/main.go
@@ -8,7 +8,7 @@ import (
 )
 
 var (
-	xmlPath    = flag.String("p", "data/import/worldofmusic.xml", "path to xml file")
+	importPath = flag.String("i", "data/import/worldofmusic.xml", "path to xml file for import")
 	exportPath = flag.String("e", "data/filtered/worldofmusic.xml", "path to export filtered xml file")
 	batchSize  = flag.Int("b", 5, "Batch size")
 	workers    = flag.Int("w", 5, "Number of data store workers")
@@ -20,6 +20,6 @@ func main() {
 	impCtx, impCancel := context.WithCancel(context.Background())
 	defer impCancel()
 
-	ldr := etl.NewLoader(etl.NewImporter(*xmlPath, *batchSize), etl.NewExporter(*exportPath), aoe.NewFilter(aoe.NewDefaultValidator()))
+	ldr := etl.NewLoader(etl.NewImporter(*importPath, *batchSize), etl.NewExporter(*exportPath), aoe.NewFilter(aoe.NewDefaultValidator()))
 	ldr.Load(impCtx, *workers)
 }
