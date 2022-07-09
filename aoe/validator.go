58000010e2e229107e67941907628233b347d4cc
Semir Mahovkic <semirm.dev@gmail.com>
2022-07-09 14:21:06 +0200

    seeba

--- aoe/validator.go
+++ aoe/validator.go
@@ -12,5 +12,5 @@ func NewDefaultValidator() *defaultValidator {
 func (val *defaultValidator) Valid(record *Record) bool {
 	releaseDate, _ := time.Parse("2006.01.02", record.ReleaseDate)
 	limitDate, _ := time.Parse("2006.01.02", "2001.01.01")
-	return len(record.TrackListing.Tracks) > 10 && releaseDate.Before(limitDate)
+	return len(record.TrackListing.Tracks) >= 10 && releaseDate.Before(limitDate)
 }
---
+++ aoe/validator_test.go
@@ -0,0 +1,73 @@
+package aoe_test
+
+import (
+	"github.com/semirm-dev/seeba/aoe"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestDefaultValidator_Valid(t *testing.T) {
+	testTable := map[string]struct {
+		given    *aoe.Record
+		expected bool
+	}{
+		"given valid record should return true": {
+			given: &aoe.Record{
+				Name:        "record 1",
+				ReleaseDate: "1999.01.15",
+				TrackListing: &aoe.TrackListing{
+					Tracks: []string{
+						"track 1", "track 2", "track 3", "track 4", "track 5",
+						"track 6", "track 7", "track 8", "track 9", "track 10",
+					},
+				},
+			},
+			expected: true,
+		},
+		"given invalid record should return false": {
+			given: &aoe.Record{
+				Name:        "record 1",
+				ReleaseDate: "2010.01.01",
+				TrackListing: &aoe.TrackListing{
+					Tracks: []string{
+						"track 1", "track 2", "track 3", "track 4", "track 5",
+					},
+				},
+			},
+			expected: false,
+		},
+		"given less than 10 tracks should return false": {
+			given: &aoe.Record{
+				Name:        "record 1",
+				ReleaseDate: "1999.01.01",
+				TrackListing: &aoe.TrackListing{
+					Tracks: []string{
+						"track 1", "track 2", "track 3", "track 4", "track 5",
+					},
+				},
+			},
+			expected: false,
+		},
+		"given record newer than 2001.01.01 should return false": {
+			given: &aoe.Record{
+				Name:        "record 1",
+				ReleaseDate: "2001.01.02",
+				TrackListing: &aoe.TrackListing{
+					Tracks: []string{
+						"track 1", "track 2", "track 3", "track 4", "track 5",
+						"track 6", "track 7", "track 8", "track 9", "track 10",
+					},
+				},
+			},
+			expected: false,
+		},
+	}
+
+	validator := aoe.NewDefaultValidator()
+	for name, suite := range testTable {
+		t.Run(name, func(t *testing.T) {
+			valid := validator.Valid(suite.given)
+			assert.Equal(t, suite.expected, valid)
+		})
+	}
+}
